36965c1b0ecbed0e4b7baea88b8a5a76b922d04a
MichelaSpina <146854245+MichelaSpina@users.noreply.github.com>
2023-10-03 17:04:06 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,15 @@
+# Here under hasghtag you can write anything you wwant and the software will not report it. it's a comment you can put. 
+# let's use R as calculator
+2 + 3
+
+# Assign to an object 
+zima <- 2 + 3
+zima
+
+
+duccio <- 5 + 3 
+duccio
+
+
+final <- duccio + zima
+final
